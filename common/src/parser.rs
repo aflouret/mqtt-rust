12bd92901766dd762622cdbeb745dec58371853b
Agustin <agusgabrielli9@gmail.com>
2021-10-24 16:59:26 -0300

    fromutf8 fix

--- common/src/parser.rs
+++ common/src/parser.rs
@@ -22,8 +22,7 @@ pub fn get_remaining_length(stream: &mut dyn Read) -> Result<u32,Box<dyn std::er
     let mut multiplier:u32 = 1;    
     let mut value:u32 = 0;
     for encoded_byte in stream.bytes() {
-        let encoded_byte = encoded_byte?;
-        //println!("byte: {}", from_utf8(&[encoded_byte])?); para el test
+        let encoded_byte: u8 = from_utf8(&[encoded_byte?])?.parse()?;
         value += (encoded_byte & 127) as u32 * multiplier;
         multiplier *= 128;
         if multiplier > 128*128*128 {
@@ -40,6 +39,6 @@ pub fn get_remaining_length(stream: &mut dyn Read) -> Result<u32,Box<dyn std::er
 // }
 fn decode_remaining_length(){
     println!("testeo {:?}", "5".as_bytes());
-    let to_test = get_remaining_length(&mut "5".as_bytes()).unwrap();
+    let to_test = get_remaining_length(&mut stdin()).unwrap();
     assert_eq!(to_test, 5);
 }// para que esto sirva hay que usar from_utf8(&[encoded_byte])?, para decodificar el as_bytes
