8c4bd7a2fbcc2a3d3aa41175c98280f190dd8a5b
Agustin <agusgabrielli9@gmail.com>
2021-10-23 23:52:47 -0300

    get_remaining_length fixed, but cannot be tested

--- common/src/parser.rs
+++ common/src/parser.rs
@@ -1,6 +1,7 @@
-use std::io::Read;
+use std::io::{BufReader, Read, stdin};
 use crate::all_packets::connect::{Connect};
 use crate::packet::{Packet, ReadPacket};
+use std::str::from_utf8;
 
 // Devuelve el packet correspondiente a lo que leyó del stream.
 pub fn read_packet(stream: &mut dyn Read) -> Result<Packet, Box<dyn std::error::Error>> {
@@ -17,21 +18,28 @@ pub fn read_packet(stream: &mut dyn Read) -> Result<Packet, Box<dyn std::error::
 
 // Algoritmo para desencodear el número que representa el RemainingLength 
 // en el fixed header de cualquier packet
-pub fn get_remaining_length(encoded_bytes: Vec<u8>) -> Result<u32,()> {
+pub fn get_remaining_length(stream: &mut dyn Read) -> Result<u32,Box<dyn std::error::Error>> {
     let mut multiplier:u32 = 1;    
     let mut value:u32 = 0;
-    for encoded_byte in encoded_bytes {
+    for encoded_byte in stream.bytes() {
+        let encoded_byte = encoded_byte?;
+        //println!("byte: {}", from_utf8(&[encoded_byte])?); para el test
         value += (encoded_byte & 127) as u32 * multiplier;
         multiplier *= 128;
         if multiplier > 128*128*128 {
-           return Err(()); }
+           return Err("Incorrect length".into()); }
         if (encoded_byte & 128) == 0 {break};
     }
     Ok(value)
 }
 
 
-/*#[test]
-fn identify_connect_package(){
-    assert_eq!(ConnectBuilder::new(), identify_package(0x10));
-}*/
+#[test]
+// fn identify_connect_package(){
+//     assert_eq!(ConnectBuilder::new(), identify_package(0x10));
+// }
+fn decode_remaining_length(){
+    println!("testeo {:?}", "5".as_bytes());
+    let to_test = get_remaining_length(&mut "5".as_bytes()).unwrap();
+    assert_eq!(to_test, 5);
+}// para que esto sirva hay que usar from_utf8(&[encoded_byte])?, para decodificar el as_bytes
