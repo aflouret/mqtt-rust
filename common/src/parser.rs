afa68cb09b76ecd299677c3ab69f4d753685f8ca
Joaquín <joahetrea@gmail.com>
2021-10-24 19:30:30 -0300

    Agrego encode_remaining_length + pruebas

--- common/src/parser.rs
+++ common/src/parser.rs
@@ -19,8 +19,8 @@ pub fn read_packet(stream: &mut dyn Read) -> Result<Packet, Box<dyn std::error::
 // Algoritmo para desencodear el número que representa el RemainingLength 
 // en el fixed header de cualquier packet
 pub fn get_remaining_length(stream: &mut dyn Read) -> Result<u32,Box<dyn std::error::Error>> {
-    let mut multiplier:u32 = 1;    
-    let mut value:u32 = 0;
+    let mut multiplier: u32 = 1;    
+    let mut value: u32 = 0;
     for encoded_byte in stream.bytes() {
         let encoded_byte: u8 = from_utf8(&[encoded_byte?])?.parse()?;
         value += (encoded_byte & 127) as u32 * multiplier;
@@ -30,6 +30,26 @@ pub fn get_remaining_length(stream: &mut dyn Read) -> Result<u32,Box<dyn std::er
         if (encoded_byte & 128) == 0 {break};
     }
     Ok(value)
+
+}
+
+fn encode_remaining_length(packet_length: u32) -> Vec<u8> {
+    let mut vec: Vec<u8> = Vec::new();
+    let mut encoded_byte;
+    let mut x = packet_length;
+    loop {
+        encoded_byte = x % 128;
+        x = x / 128;
+        if x > 0 {
+            encoded_byte = encoded_byte | 128;
+        }
+        vec.push(encoded_byte as u8);
+        if x <= 0 {
+            break;
+        }
+    }
+
+    vec
 }
 
 
@@ -41,4 +61,53 @@ fn decode_remaining_length(){
     println!("testeo {:?}", "5".as_bytes());
     let to_test = get_remaining_length(&mut stdin()).unwrap();
     assert_eq!(to_test, 5);
+    
 }// para que esto sirva hay que usar from_utf8(&[encoded_byte])?, para decodificar el as_bytes
+
+#[test]
+fn encode_length_1_byte_min(){
+    let to_test = encode_remaining_length(1);
+    assert_eq!(to_test, [1]);
+}
+
+#[test]
+fn encode_length_1_byte_max(){
+    let to_test = encode_remaining_length(127);
+    assert_eq!(to_test, [127]);
+}
+
+#[test]
+fn encode_length_2_bytes_min(){
+    let to_test = encode_remaining_length(128);
+    assert_eq!(to_test, [128, 1]);
+}
+
+#[test]
+fn encode_length_2_bytes_max(){
+    let to_test = encode_remaining_length(16383);
+    assert_eq!(to_test, [255, 127]);
+}
+
+#[test]
+fn encode_length_3_bytes_min(){
+    let to_test = encode_remaining_length(16384);
+    assert_eq!(to_test, [128, 128, 1]);
+}
+
+#[test]
+fn encode_length_3_bytes_max(){
+    let to_test = encode_remaining_length(2097151);
+    assert_eq!(to_test, [255, 255, 127]);
+}
+
+#[test]
+fn encode_length_4_bytes_min(){
+    let to_test = encode_remaining_length(2097152);
+    assert_eq!(to_test, [128, 128, 128, 1]);
+}
+
+#[test]
+fn encode_length_4_bytes_max(){
+    let to_test = encode_remaining_length(268435455);
+    assert_eq!(to_test, [255, 255, 255, 127]);
+}
