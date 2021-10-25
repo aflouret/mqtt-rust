b6c9dda6d5f3f2f8f21bd866236b79972e88fdbe
Agustin <agusgabrielli9@gmail.com>
2021-10-25 10:58:42 -0300

    sending connect and receiving connack from client

--- client/src/main.rs
+++ client/src/main.rs
@@ -5,15 +5,12 @@ use common::parser;
 use common::packet::Packet;
 
 // Para usar cualquier funcion/cosa de common, hacemos "common::archivo::algo"
-fn main() -> Result<(), ()> {
+fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let address = "127.0.0.1:8080";
     println!("Conectándome a {:?}", address);
     
-    // Para probar la conexión entre cliente-servidor, hicimos que desde el cliente
-    // se escriba por stdin, se lo mande al server y que este lo imprima. En el futuro
-    // le vamos a estar enviando packets como el Connect. 
-    client_run(&address).unwrap();
+    client_run(&address)?; // misma duda de cómo hacer el main que en server
     Ok(())
 }
 
@@ -21,12 +18,12 @@ fn client_run(address: &str) -> Result<(), Box<dyn std::error::Error>> {
     let mut socket = TcpStream::connect(address)?;
 
     let connect_packet = Connect::new("pepito".to_owned(), "u".to_owned(), "p".to_owned(), "connect_flags".to_owned(), "last_will_message".to_owned(), "last_will_topic".to_owned());
-
-    //connect_packet.write_to(&mut socket)?;
+    connect_packet.write_to(&mut socket)?;
+    println!("Se envió el connect packet");
     
     let received_packet = parser::read_packet(&mut socket)?;
     if let Packet::Connack(connack_packet) = received_packet {
-        println!("Received connack packet. Session present: {}. Connect return code: {}", connack_packet.session_present, connack_packet.connect_return_code);
+        println!("Se recibió el connack packet. Session present: {}. Connect return code: {}", connack_packet.session_present, connack_packet.connect_return_code);
     }
 
     Ok(())
