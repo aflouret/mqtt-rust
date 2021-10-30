7b7fb1a62c2210c2121ea99f2062b375385ef62f
ArturoAguilarL <arturo.aguilar@mercadolibre.com>
2021-10-30 19:00:39 -0300

    refactor server/main , add config

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,34 +1,15 @@
-mod server;
+use crate::config::Config;
+use crate::server::Server;
 
-use common::all_packets::connack::Connack;
-use common::packet::WritePacket;
-use common::parser;
-use std::net::{TcpListener, TcpStream};
+mod server;
+mod config;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let address = "0.0.0.0:8080".to_owned();
-    println!("Esuchando en {:?}", &address);
-
-    server_run(&address)?;
+/*    let address = "0.0.0.0:8080".to_owned();
+    println!("Esuchando en {:?}", &address);*/
+    let config = Config::new();
+    let server = Server::new(config)?;
+    server.server_run()?;
     Ok(())
 }
 
-fn server_run(address: &str) -> Result<(), Box<dyn std::error::Error>> {
-    let listener = TcpListener::bind(address)?;
-    let connection = listener.accept()?;
-    let mut client_stream: TcpStream = connection.0;
-    handle_client(&mut client_stream)?;
-    Ok(())
-}
-
-// Leemos el connect packet y enviamos el connack desde el TcpStream.
-fn handle_client(stream: &mut TcpStream) -> Result<(), Box<dyn std::error::Error>> {
-    let _received_packet = parser::read_packet(stream)?;
-    println!("Se recibió el connect packet");
-
-    let connack_packet = Connack::new(false, 0);
-    connack_packet.write_to(stream)?;
-    println!("Se envió el connack packet");
-
-    Ok(())
-}
--- server/src/server.rs
+++ server/src/server.rs
@@ -1,1 +1,47 @@
+use std::io;
+use std::net::{TcpListener, TcpStream};
+use common::all_packets::connack::Connack;
+use common::packet::WritePacket;
+use common::parser;
+use crate::config::Config;
 
+pub struct Server {
+
+    config: Config
+}
+
+impl Server {
+    pub fn new(config: Config) -> io::Result<Self>{
+        Ok(Self {
+            config,
+        })
+    }
+
+
+    pub fn server_run(mut self) -> Result<(), Box<dyn std::error::Error>> {
+        let address = self.config.get_address() + &*self.config.get_port();
+        println!(" address {} ",address);
+        self.run(&address);
+        Ok(())
+    }
+
+    fn run(self, address: &str) -> io::Result<()> {
+        let listener = TcpListener::bind(address)?;
+        let connection = listener.accept()?;
+        let mut client_stream: TcpStream = connection.0;
+        Server::handle_client(&mut client_stream)?;
+        Ok(())
+    }
+
+    // Leemos el connect packet y enviamos el connack desde el TcpStream.
+    fn handle_client(stream: &mut TcpStream) -> io::Result<()> {
+/*        let _received_packet = parser::read_packet(stream)?;*/
+        println!("Se recibió el connect packet");
+
+/*        let connack_packet = Connack::new(false, 0);
+        connack_packet.write_to(stream)?;*/
+        println!("Se envió el connack packet");
+
+        Ok(())
+    }
+}
