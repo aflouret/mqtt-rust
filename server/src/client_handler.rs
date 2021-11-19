653e30380fd32539125d81abbd3ad5ba29c2c081
Agustin <aflouret@gmail.com>
2021-11-19 01:46:55 -0300

    refactor client handler

--- server/src/client_handler.rs
+++ server/src/client_handler.rs
@@ -1,24 +1,94 @@
 use std::net::TcpStream;
-use common::all_packets::connect::Connect;
-use crate::session::Session;
-use std::sync::mpsc::{Sender, Receiver};
-use common::packet::{Packet, ReadPacket, WritePacket};
-use std::io::{Read, Write};
+use common::packet::{Packet, WritePacket};
 use common::parser;
+use std::sync::{Mutex, mpsc};
+use std::sync::mpsc::{Receiver, Sender};
+use std::thread::{self, JoinHandle};
+use std::sync::{RwLock, Arc};
+use std::collections::HashMap;
+
+pub struct ClientHandler {
+    id: u32,
+    stream: Option<TcpStream>,
+    sender: Option<Sender<(u32, Result<Packet,Box<dyn std::error::Error + Send>>)>>,
+    receiver: Option<Receiver<Result<Packet,Box<dyn std::error::Error + Send>>>>,
+}
+
+impl ClientHandler {
+    pub fn new(
+        id: u32, 
+        stream: TcpStream, 
+        senders_to_c_h_writers: Arc<RwLock<HashMap<u32, Arc<Mutex<Sender<Result<Packet,Box<dyn std::error::Error + Send>>>>>>>>,
+        sender:Sender<(u32, Result<Packet,Box<dyn std::error::Error + Send>>)>,
+    ) -> ClientHandler {
+        
+        let (server_tx, c_h_writer_rx) = mpsc::channel::<Result<Packet,Box<dyn std::error::Error + Send>>>();
+        let mut hash = senders_to_c_h_writers.write().unwrap();
+        hash.insert(id, Arc::new(Mutex::new(server_tx)));
+        
+        ClientHandler {
+            id,
+            stream: Some(stream),
+            sender: Some(sender),
+            receiver: Some(c_h_writer_rx),
+        }
+    }
+
+    pub fn run(mut self) -> Result<JoinHandle<()>, Box<dyn std::error::Error>>{
+
+        let stream = self.stream.take().unwrap();
+
+
+        let receiver = self.receiver.take().unwrap();
+        let sender = self.sender.take().unwrap();
+
+        let mut client_handler_writer = ClientHandlerWriter::new(stream.try_clone()?, receiver);
+        let mut client_handler_reader = ClientHandlerReader::new(self.id, stream, sender);
+
+        let writer_join_handle = thread::spawn(move || {
+
+            // channel para que el reader le avise al writer que el cliente se desconecto
+            let (error_tx, error_rx) = mpsc::channel();
+
+            let reader_join_handle = thread::spawn(move || {
+                loop {
+                    let result = client_handler_reader.receive_packet();
+                    if result.is_err() {
+                        error_tx.send(result).unwrap();
+                        break;
+                    }
+                    error_tx.send(Ok(())).unwrap();
+                }
+            });
+            
+            loop {
+                if let Ok(result) = error_rx.try_recv() {
+                    if result.is_err() {
+                        break;
+                    }
+                }
+                client_handler_writer.send_packet().unwrap();
+            }
+
+            reader_join_handle.join().unwrap();
+        });
+
+        Ok(writer_join_handle)
+    }
+}
+
 
 //LEE EN CHANNEL, ESCRIBE EN SOCKET
-pub struct ClientHandlerWriter {
+struct ClientHandlerWriter {
     //Maneja la conexion del socket
-    id: u32,
     socket: TcpStream,
     //TODO:Option<Tcp> -> CONSULTAR:no iria porque  si se desconecta se destruye el client_handler
     receiver: Receiver<Result<Packet,Box<dyn std::error::Error + Send>>>, //Por acá recibe los paquetes que escribe en el socket
 }
 
 impl ClientHandlerWriter {
-    pub fn new(id: u32, socket: TcpStream, receiver: Receiver<Result<Packet,Box<dyn std::error::Error + Send>>>) -> ClientHandlerWriter {
+    pub fn new(socket: TcpStream, receiver: Receiver<Result<Packet,Box<dyn std::error::Error + Send>>>) -> ClientHandlerWriter {
         ClientHandlerWriter {
-            id,
             socket,
             receiver,
         }
@@ -56,11 +126,9 @@ impl ClientHandlerWriter {
 }
 
 //LEE DE SOCKET, ESCRIBE EN CHANNEL
-pub struct ClientHandlerReader {
-    //Maneja la conexion del socket
+struct ClientHandlerReader {
     id: u32,
     socket: TcpStream,
-    //TODO:Option<Tcp> -> CONSULTAR:no iria porque  si se desconecta se destruye el client_handler
     sender: Sender<(u32, Result<Packet,Box<dyn std::error::Error + Send>>)>,//Por acá manda paquetes al sv
 }
 
--- server/src/server.rs
+++ server/src/server.rs
@@ -1,14 +1,13 @@
 use crate::config::Config;
-use common::packet::{Packet, WritePacket};
+use common::packet::{Packet};
 use std::io;
-use std::net::{TcpListener, TcpStream};
+use std::net::{TcpListener};
 use std::collections::HashMap;
-use crate::client_handler::{ClientHandlerReader, ClientHandlerWriter};
-use crate::packet_processor::{PacketProcessor};
+use crate::client_handler::{ClientHandler};
+use crate::packet_processor::PacketProcessor;
 
-use std::sync::{Mutex, MutexGuard, mpsc};
-use std::sync::mpsc::{Receiver, Sender};
-use std::thread;
+use std::sync::{Mutex, mpsc};
+use std::sync::mpsc::{Sender};
 use std::sync::{RwLock, Arc};
 
 
@@ -30,7 +29,7 @@ impl Server {
         let (c_h_reader_tx, server_rx) = mpsc::channel::<(u32, Result<Packet,Box<dyn std::error::Error + Send>>)>();
 
         let packet_processor = PacketProcessor::new(server_rx, senders_to_c_h_writers.clone());
-        packet_processor.run();
+        packet_processor.run()?;
 
         self.handle_connections(listener, senders_to_c_h_writers, c_h_reader_tx);
         
@@ -39,43 +38,22 @@ impl Server {
 
     fn handle_connections(&self, listener: TcpListener, senders_to_c_h_writers:  Arc<RwLock<HashMap<u32, Arc<Mutex<Sender<Result<Packet,Box<dyn std::error::Error + Send>>>>>>>>, c_h_reader_tx: Sender<(u32, Result<Packet,Box<dyn std::error::Error + Send>>)>) {
         let mut id: u32 = 0;
+        let mut join_handles = vec![];
+
         for stream in listener.incoming() {
-            if let Ok(client_stream) = stream {
-                let (server_tx, c_h_writer_rx) = mpsc::channel::<Result<Packet,Box<dyn std::error::Error + Send>>>();
-                let mut hash = senders_to_c_h_writers.write().unwrap();
-                hash.insert(id, Arc::new(Mutex::new(server_tx)));
+            if let Ok(stream) = stream {
+                let client_handler = ClientHandler::new(id, stream, senders_to_c_h_writers.clone(), c_h_reader_tx.clone());
+                
+                if let Ok(join_handle) = client_handler.run() {
+                    join_handles.push(join_handle);
+                };
+
                 id += 1;
-
-                let mut client_handler_writer = ClientHandlerWriter::new(id, client_stream.try_clone().unwrap(), c_h_writer_rx);
-                let mut client_handler_reader = ClientHandlerReader::new(id, client_stream, c_h_reader_tx.clone());
-
-                thread::spawn(move || {
-
-                    let (error_tx, error_rx) = mpsc::channel();
-    
-                    let handler_reader = thread::spawn(move || {
-                        loop {
-                            let result = client_handler_reader.receive_packet();
-                            if result.is_err() {
-                                error_tx.send(result).unwrap();
-                                break;
-                            }
-                            error_tx.send(Ok(())).unwrap();
-                        }
-                    });
-                    
-                    loop {
-                        if let Ok(result) = error_rx.try_recv() {
-                            if result.is_err() {
-                                break;
-                            }
-                        }
-                        client_handler_writer.send_packet().unwrap();
-                    }
-    
-                    handler_reader.join().unwrap();
-                });
             }
+        }
+
+        for handle in join_handles {
+            handle.join().unwrap();
         }
     }
     
