901295c3491695de999eb45b65897f2db0ca36f3
Agustin <aflouret@gmail.com>
2021-11-26 21:30:05 -0300

    added '+' wildcard

--- server/src/topic_filters.rs
+++ server/src/topic_filters.rs
@@ -4,20 +4,34 @@ pub fn filter_matches_topic(filter: &str, topic_name: &str) -> bool {
         return filter == topic_name;
     }
 
-    if filter.contains('#') {
-        if filter == "#" {
-            return true;
-        }
-
-        let filter_prefix = filter.trim_end_matches('#');
-        if topic_name == &filter_prefix[..filter_prefix.len()-1] {
-            return true;
-        }
-        return topic_name.starts_with(filter_prefix);
+    if filter == "#" {
+        return true;
     }
 
-    return false;
+    let filter_levels: Vec<&str> = filter.split('/').collect();
+    let topic_name_levels: Vec<&str> = topic_name.split('/').collect();
+
+    if let Some(index)= filter.split('/').position(|l| l == "#") {
+        return match_levels(&filter_levels[..index], &topic_name_levels[..index]);
+    }
+
+    return match_levels(&filter_levels, &topic_name_levels);
 }
+
+fn match_levels(filter: &[&str], topic: &[&str]) -> bool {
+
+    if filter.len() != topic.len() {
+        return false;
+    }
+
+    for (pos, level) in filter.iter().enumerate() {
+        if topic[pos] != *level && *level != "+" {
+            return false;
+        }
+    }
+    return true;
+}
+
 
 #[cfg(test)]
 mod tests {
@@ -78,5 +92,78 @@ mod tests {
         assert!(filter_matches_topic("#", "abc/def"))
     }
 
+    #[test]
+    fn test12() {
+        assert!(filter_matches_topic("#", "/"))
+    }
+
+    #[test]
+    fn test13() {
+        assert!(filter_matches_topic("/#", "/"))
+    }
+
+    #[test]
+    fn test14() {
+        assert!(filter_matches_topic("#", "/abc/def"))
+    }
+
+    #[test]
+    fn test15() {
+        assert!(filter_matches_topic("/#", "/abc/def"))
+    }
+
+    #[test]
+    fn test16() {
+        assert!(filter_matches_topic("abc/+", "abc/def"))
+    }
     
+    #[test]
+    fn test17() {
+        assert!(filter_matches_topic("abc/+/ghi", "abc/def/ghi"))
+    }
+
+    #[test]
+    fn test18() {
+        assert!(filter_matches_topic("abc/+/ghi/+", "abc/def/ghi/jkl"))
+    }
+
+    #[test]
+    fn test19() {
+        assert!(filter_matches_topic("abc/+/#", "abc/def/ghi/jkl"))
+    }
+
+    #[test]
+    fn test20() {
+        assert!(filter_matches_topic("+/def/+/#", "abc/def/ghi/jkl"))
+    }
+
+    #[test]
+    fn test21() {
+        assert!(filter_matches_topic("+", "abc"))
+    }
+
+    #[test]
+    fn test22() {
+        assert!(filter_matches_topic("/+", "/abc"))
+    }
+
+    #[test]
+    fn test23() {
+        assert!(filter_matches_topic("+/+", "/abc"))
+    }
+
+    #[test]
+    fn test24() {
+        assert!(!filter_matches_topic("+", "/abc"))
+    }
+
+    #[test]
+    fn test25() {
+        assert!(!filter_matches_topic("+", "abc/def"))
+    }
+
+    #[test]
+    fn test26() {
+        assert!(!filter_matches_topic("+", "abc/"))
+    }
 }
