749147d073274472c1a340d507863f3ca51214d9
Joaquín <joahetrea@gmail.com>
2021-12-14 15:48:43 -0300

    Added unit tests for authenticator

--- server/src/authenticator.rs
+++ server/src/authenticator.rs
@@ -7,6 +7,10 @@ pub struct Authenticator{
 }
 
 impl Authenticator {
+    fn new(accounts: HashMap<String, String>) -> Authenticator {
+        Authenticator {accounts}
+    }
+
     pub fn from(filename: String) -> Result<Authenticator, Error> {
         let mut hash: HashMap<String, String> = HashMap::new();
         let file = File::open(filename)?;
@@ -27,7 +31,7 @@ impl Authenticator {
             hash.insert(vec.get(0).unwrap().to_string(), vec.get(1).unwrap().to_string());
         }
 
-        Ok(Authenticator { accounts: hash })
+        Ok(Authenticator::new(hash))
     }
 
     pub fn account_is_valid(&self, username: &String, password: &String) -> bool {
@@ -35,5 +39,46 @@ impl Authenticator {
             None => false,
             Some(pass) => pass == password,
         }
+    }
+}
+
+/* ------------------------------------------- */
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::io::{Cursor, Read, Write};
+
+    #[test]
+    fn valid_account_returns_true() {
+        let mut hash: HashMap<String, String> = HashMap::new();
+        hash.insert("usuario1".to_string(), "contraseña1".to_string());
+        hash.insert("usuario2".to_string(), "contraseña2".to_string());
+        hash.insert("usuario3".to_string(), "contraseña3".to_string());
+        let authenticator = Authenticator::new(hash);
+        let to_test = authenticator.account_is_valid(&"usuario2".to_string(), &"contraseña2".to_string());
+        assert_eq!(to_test, true);
+    }
+
+    #[test]
+    fn username_not_existing_returns_false() {
+        let mut hash: HashMap<String, String> = HashMap::new();
+        hash.insert("usuario1".to_string(), "contraseña1".to_string());
+        hash.insert("usuario2".to_string(), "contraseña2".to_string());
+        hash.insert("usuario3".to_string(), "contraseña3".to_string());
+        let authenticator = Authenticator::new(hash);
+        let to_test = authenticator.account_is_valid(&"usuario4".to_string(), &"contraseña4".to_string());
+        assert_eq!(to_test, false);
+    }
+
+    #[test]
+    fn incorrect_password_returns_false() {
+        let mut hash: HashMap<String, String> = HashMap::new();
+        hash.insert("usuario1".to_string(), "contraseña1".to_string());
+        hash.insert("usuario2".to_string(), "contraseña2".to_string());
+        hash.insert("usuario3".to_string(), "contraseña3".to_string());
+        let authenticator = Authenticator::new(hash);
+        let to_test = authenticator.account_is_valid(&"usuario2".to_string(), &"contraseña3".to_string());
+        assert_eq!(to_test, false);
     }
 }
