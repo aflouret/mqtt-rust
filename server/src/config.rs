98b6b6c21c17b7b51a231ebf65d30181ae65fd14
Agustin <aflouret@gmail.com>
2021-12-20 03:11:32 -0300

    added config file

--- server/src/config.rs
+++ server/src/config.rs
@@ -1,4 +1,4 @@
-use std::env;
+use std::{env, fs::File, io::{BufReader, BufRead}};
 
 const DEFAULT_LOGFILE: &str = "logfile.txt";
 const DEFAULT_PORT: u16 = 8080;
@@ -11,30 +11,44 @@ pub struct Config {
 }
 
 impl Config {
-    pub fn new(mut args: env::Args) ->  Result<Config, String>  {
+    pub fn new(mut args: env::Args) ->  Result<Config, Box<dyn std::error::Error>>  {
         
         args.next(); //args[0] no nos interesa, es el nombre del programa
 
-        let port = match args.next() {
-            Some(arg) => match str::parse::<u16>(&arg) {
-                Ok(port) => port,
-                Err(_) => return Err("El puerto no es valido".into())
-            },
-            None => DEFAULT_PORT,
-        };
+        if let Some(config_file_path) = args.next() {
+            let config_file = File::open(config_file_path)?;
+            let reader = BufReader::new(config_file);
+            let mut lines = reader.lines();
+            
+            let port = match lines.next() {
+                Some(Ok(arg)) => match str::parse::<u16>(&arg) {
+                    Ok(port) => port,
+                    Err(_) => return Err("El puerto no es valido".into())
+                },
+                Some(Err(_)) => return Err("El puerto no es valido".into()),
+                None => DEFAULT_PORT,
+            };
+    
+            let log_filename = match lines.next() {
+                Some(Ok(arg)) => arg,
+                Some(Err(_)) => return Err("El logfile no es valido".into()),
+                None => DEFAULT_LOGFILE.to_string(),
+            };
 
-        let log_filename = match args.next() {
-            Some(arg) => arg,
-            None => DEFAULT_LOGFILE.to_string(),
-        };
-        
+            return Ok(Config {
+                port,
+                address: DEFAULT_ADDRESS.to_string(),
+                log_filename
+            });
+        }
+
         Ok(Config {
-            port,
+            port: DEFAULT_PORT,
             address: DEFAULT_ADDRESS.to_string(),
-            //Path de archivo sobre el cuál se realizará un dump
-            //Intervalo de tiempo para el cual se realizará el dump
-            log_filename
+            log_filename: DEFAULT_LOGFILE.to_string(),
         })
+        
+        
     }
 }
 
