7a3c9e99235a9de49a916a8227b417f8a3847d07
ArturoAguilarL <arturo.aguilar@mercadolibre.com>
2022-01-18 22:07:39 -0300

    publico branch

--- random-client/src/main.rs
+++ random-client/src/main.rs
@@ -1,3 +1,9 @@
+/*Parte A) Implementar un programa que simule la generación de datos de un dispositivo (por ejemplo,
+la medición de la temperatura ambiente) cada determinado tiempo y lo publique como cliente al
+ servidor MQTT utilizando un tópico predefinido. Este programa puede ser de consola, sin necesidad
+  de interfaz gráfica, ni de interacción con el usuario.
+Se permite utilizar el crate rand para la generación de valores.
+*/
 fn main() {
     println!("Hello, world!");
 }
