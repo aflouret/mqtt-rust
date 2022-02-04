89c25be56fc56f9e16ffa5a2bca57943cbc70df1
Joaquín <joahetrea@gmail.com>
2022-02-03 21:15:11 -0300

    Added connection to mqtt server and first version of publishing data

--- http-server/src/main.rs
+++ http-server/src/main.rs
@@ -16,22 +16,67 @@ use std::io::prelude::*;
 use std::net::TcpListener;
 use std::net::TcpStream;
 use std::fs;
+use std::thread;
+use common::packet::{Qos};
+use std::sync::{mpsc, Mutex};
+use std::sync::Arc;
+mod mqtt_client;
 
-const HTML_PATH: &str = "http-server/src/publishing_page.html";
+const HTML_PATH: &str = "src/publishing_page.html";
 const IP: &str = "0.0.0.0";
 const PORT: &str = "8081";
+const IP_MQTT: &str = "0.0.0.0";
+const PORT_MQTT: &str = "8080";
+const TOPIC_MQTT: &str = "topica";
+const QOS_MQTT: Qos = Qos::AtMostOnce;
 
-fn main() {
+pub const HEADER: &str = r#"<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>PublishingPage</title>
+  </head>
+"#;
+
+pub const FOOTER: &str = r#"
+</html>
+"#;
+
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let (sender, receiver) = mpsc::channel::<String>();
+    let mut client = mqtt_client::MQTTClient::new(sender);
+    client.connect_to(IP_MQTT.to_string() + ":" + PORT_MQTT)?;
+    println!("Me conecté con exito");
+    client.subscribe_to(TOPIC_MQTT.to_string(), QOS_MQTT)?;
+    println!("Me subscribí con exito a: {}", TOPIC_MQTT);
+    client.run();
+
+    let body = Arc::new(Mutex::new("    <h1>Current Temperature:</h1>".to_string()));
+    let body_clone = body.clone();
+    thread::spawn(move || {
+        loop {
+            if let Ok(message) = receiver.recv(){
+                body_clone.lock().unwrap().push_str(&format!("\n        <p>{}</p>", &message));
+            }
+        }
+    });
+
     let listener = TcpListener::bind(IP.to_string() + ":" + PORT).unwrap();
 
     for stream in listener.incoming() {
+        let b = body.clone();
         let stream = stream.unwrap();
 
-        handle_connection(stream);
+        thread::spawn(move || {
+            handle_connection(stream, b);
+        });
     }
+
+    Ok(())
 }
 
-fn handle_connection(mut stream: TcpStream) {
+fn handle_connection(mut stream: TcpStream, body: Arc<Mutex<String>>) {
     let mut buffer = [0; 1024];
 
     stream.read(&mut buffer).unwrap();
@@ -40,7 +85,10 @@ fn handle_connection(mut stream: TcpStream) {
     println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
 
     // Generamos la response con la temperatura que nos llegó del client MQTT
-    let html_in_string = fs::read_to_string(HTML_PATH).unwrap();
+    //let html_in_string = fs::read_to_string(HTML_PATH).unwrap();
+
+    let html_in_string = HEADER.to_string() + &get_body(&body.lock().unwrap().clone()) + FOOTER;
+    println!("el html actual es: {}", html_in_string);
     let response = format!(
         "HTTP/1.1 200 OK\r\nContent-Length: {}\r\n\r\n{}",
         html_in_string.len(),
@@ -49,4 +97,13 @@ fn handle_connection(mut stream: TcpStream) {
 
     stream.write(response.as_bytes()).unwrap();
     stream.flush().unwrap();
+}
+
+pub fn get_body(body: &str) -> String {
+    format!(
+        r#"  <body>
+    {}
+  </body>"#,
+        body
+    )
 }
---
+++ http-server/src/mqtt_client.rs
@@ -0,0 +1,105 @@
+use common::all_packets::connect::{Connect, ConnectPayload};
+use common::all_packets::connack::{CONNACK_CONNECTION_ACCEPTED};
+use common::all_packets::subscribe::{Subscribe};
+use common::all_packets::suback::{SubackReturnCode};
+use common::packet::{WritePacket, Packet, Qos, Subscription};
+use std::io::{Error, ErrorKind};
+use std::net::TcpStream;
+use std::thread;
+use std::sync::mpsc::Sender;
+
+const ERROR_NOT_CONNECTED: &str = "The client is not connected";
+const ERROR_IN_CONNECTION: &str = "Connection not accepted";
+const ERROR_CONNACK_NOT_RECEIVED: &str = "Didn't received the connack packet";
+const ERROR_SUBACK_NOT_RECEIVED: &str = "Didn't received the suback packet";
+const ERROR_FAILED_SUBSCRIPTION: &str = "Failure in suback return code";
+
+pub struct MQTTClient {
+    socket: Option<TcpStream>,
+    sender: Sender<String>
+}
+
+impl MQTTClient {
+    pub fn new(sender: Sender<String>) -> MQTTClient{
+        MQTTClient {
+            socket: None,
+            sender
+        }
+    }
+
+    pub fn connect_to(&mut self, address: String) -> Result<(), Box<dyn std::error::Error>>{
+        let mut socket = TcpStream::connect(address)?;
+        
+        let connect_packet = Connect::new(
+            ConnectPayload::new(
+                "serverhttp".to_string(),
+                None,
+                None,
+                None,
+                None,
+            ),
+            0,
+            false,
+            false,
+            false,
+        );
+        
+        connect_packet.write_to(&mut socket)?;
+    
+        let connack_packet = Packet::read_from(&mut socket)?;
+        match connack_packet {
+            Packet::Connack(connack) => {
+                if connack.connect_return_code != CONNACK_CONNECTION_ACCEPTED {
+                    return Err(Box::new(Error::new(ErrorKind::Other, ERROR_IN_CONNECTION)))
+                }
+                println!("Connack received!");
+            },
+            _ => return Err(Box::new(Error::new(ErrorKind::Other, ERROR_CONNACK_NOT_RECEIVED))),
+        }
+    
+        self.socket = Some(socket);
+        Ok(())
+    }
+
+    pub fn subscribe_to(&mut self, topic: String, qos: Qos) -> Result<(), Box<dyn std::error::Error>> {
+        if let Some(socket) = &mut self.socket {
+            let mut subscribe = Subscribe::new(73);
+            subscribe.add_subscription(Subscription {
+                topic_filter: topic,
+                max_qos: qos,
+            });
+
+            subscribe.write_to(socket)?;
+
+            let suback = Packet::read_from(socket)?;
+            match suback {
+                Packet::Suback(suback) => {
+                    println!("Suback received!");
+                    if suback.return_codes.get(0).unwrap() == &SubackReturnCode::Failure {
+                        return Err(Box::new(Error::new(ErrorKind::Other, ERROR_FAILED_SUBSCRIPTION)));
+                    }
+
+
+                },
+                _ => return Err(Box::new(Error::new(ErrorKind::Other, ERROR_SUBACK_NOT_RECEIVED))),
+            }
+
+            return Ok(());
+        }
+
+        Err(Box::new(Error::new(ErrorKind::Other, ERROR_NOT_CONNECTED)))
+    }
+
+    pub fn run(self){
+        thread::spawn(|| {
+            if let Some(mut socket) = self.socket {
+                loop {
+                    if let Ok(Packet::Publish(publish)) = Packet::read_from(&mut socket){
+                        self.sender.send(publish.application_message).unwrap();
+                        //TODO: enviar puback en caso de qos 1?
+                    }
+                }
+            }
+        });
+    }
+}
